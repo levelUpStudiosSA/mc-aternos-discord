e5c095e9977566c38cd5e0e10801309fa1236706
levelUpStudiosSA <Mesharyalharby114@gmail.com>
2026-07-21 01:23:33 +0300

    aternos_server_bot.py

--- aternos_server_bot.py
+++ aternos_server_bot.py
@@ -1,7 +1,6 @@
 import discord
 import os
 from python_aternos import Client
-import time
 import asyncio
 
 TOKEN = os.environ.get("DISCORD_TOKEN")
@@ -10,35 +9,26 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-aternos = Client.from_credentials('discord1bot', 'MTUyODc5NjQ3Njg2MjM3MDA4Mw.G4q7qc.uEOV4v09JCtZYEWBd61CpR2uEbDUboHUeKCBIY')
+ATERNOS_COOKIE = "WW5uvr7D7dmD8Me40YQEUfEgOv94b7hKKIpyrk9SeDYnnYb6wRFQJ6FmjwpaRmtpMmVnhA25eqmJ82JxkLwAJ9PFf0PRbV9g7nnn"
+
+aternos = Client.from_cookie(ATERNOS_COOKIE)
 atservers = aternos.list_servers()
 myserv = atservers[0]
 
 @client.event
 async def on_ready():
-    print('we have logged in as {0.user}'.format(client))
+    print('Bot is ready!')
 
 @client.event
 async def on_message(message):
-    username = message.author.name 
-    user_message = str(message.content)
-    
-    if isinstance(message.channel, discord.TextChannel):
-        channel = message.channel.name
-    else:
-        channel = "DM"
-        
-    print(f'{username}: {user_message} ({channel})')
-
     if message.author == client.user:
         return
 
-    if channel == 'bot-cmnds':
-        if user_message.lower() == '?hello':
-            await message.channel.send(f'Hello {username}!')
-            return
-
-        if user_message.lower() == '?server_start':
+    if isinstance(message.channel, discord.TextChannel) and message.channel.name == 'bot-cmnds':
+        if message.content.lower() == '?hello':
+            await message.channel.send(f'Hello {message.author.name}!')
+        
+        elif message.content.lower() == '?server_start':
             myserv.start()
             while True:
                 ping = str(os.popen('mcstatus OGZsmp.aternos.me status | grep description').read())
@@ -46,12 +36,10 @@ async def on_message(message):
                     await asyncio.sleep(1)
                 else:
                     break
-            await message.channel.send("server is now alive!!! you can join in 2-3 minutes by pasting ||OGZsmp.aternos.me:50529|| in the server address.")
-            return
-
-        if user_message.lower() == '?server_stop':
+            await message.channel.send("Server is starting! Join in 2-3 mins: OGZsmp.aternos.me:50529")
+            
+        elif message.content.lower() == '?server_stop':
             myserv.stop()
-            await message.channel.send('server stopped')
-            return
+            await message.channel.send('Server stopped')
 
 client.run(TOKEN)
